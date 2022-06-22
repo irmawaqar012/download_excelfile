df3f60994d226bb7ec0303b1fe2af7eba5082c05
Irma Waqar Abbasi <89778552+irmawaqar012@users.noreply.github.com>
2022-06-22 12:04:16 +0500

    automate relative path

--- practice.py
+++ practice.py
@@ -1,16 +1,16 @@
+from lib2to3.pgen2.driver import Driver
 from os import path
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-# import openpyxl
+import openpyxl as O
 
 chromeOptions = webdriver. ChromeOptions()
-prefs = {"download.default_directory" : "E:\\PythonPractice\\download_excelfile"}
-# C:\\Users\\Aamir Trader\'s\\Downloads
-chromeOptions. add_experimental_option("prefs",prefs)
-# chromedriver = "path/to/chromedriver.exe"
-# driver = webdriver.Chrome(executable_path="E:\PythonPractice\download_excelfile", options = chromeOptions)
+folder_path=os.path.dirname(os.path.abspath(__file__))
 
+prefs = {"download.default_directory" : folder_path}
+chromeOptions. add_experimental_option("prefs",prefs)
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), options = chromeOptions)
 
@@ -28,37 +28,18 @@ time.sleep(0.2)
 driver.find_element_by_id("Button1").click()
 time.sleep(1)
 
-
-# driver.maximize_window()
-
 # download excel file
 driver.find_element_by_id("txtReport").click()
 time.sleep(0.5)
 driver.find_element_by_id("txtMonthly").click()
 time.sleep(0.5)
 driver.find_element_by_id("MainContent_Button15").click()
-time.sleep(60)
-
-# path=  "C:\Users\Aamir Trader's\Downloads\Data_inOut.xls"
-# wb=openpyxl.load_workbook(path)
-# sheet=wb.active
-# rows=sheet.max_row
-# cols=sheet.max_column
-
-# for r in range(1, rows+1):
-#     for c in range(1, cols+1):
-#         print(sheet.cell(row=r, cloumn=c).value, end="   ")
-#         print()
-
-# print(rows)
-# print(cols)
-# f = open("MainContent_Button15", "r")
-# time.sleep(3)
-# print(f)
-# time.sleep(3)
+time.sleep(30)
 
 
-  
+
+
 
 driver.close()
+driver.quit()
 
